88c57af691907f0541dae7dad12975c9c1de0709
Marcos Gaeta <mgaeta89@gmail.com>
2022-12-08 14:58:42 -0800

    nicer variable names

--- y22/src/problems/p08.py
+++ y22/src/problems/p08.py
@@ -6,7 +6,7 @@ from y22.src.utils import io
 def main():
     current_day = io.get_day()
 
-    for test in [False]:
+    for test in [True, False]:
         filename = io.get_input_filename(2022, current_day, test=test)
         raw_input = io.get_inputs_raw(filename)
         board, width, length = parse_board(raw_input)
@@ -14,71 +14,71 @@ def main():
         print(treehouse_2(board, width, length))
 
 
-def parse_board(raw_input):
+def parse_board(raw_input) -> tuple[list[int], int, int]:
     board = []
     width = None
-    for i, row in enumerate(raw_input):
+    length = 0
+    for row in raw_input:
         if row == "":
             continue
 
+        length += 1
         width = len(row)
         for j in list(row):
             board.append(int(j))
 
-    return board, width, len(raw_input),
+    return board, width, length
 
 
-def get_all_neighbors(width, length, row, column):
+def get_index(width: int, row: int, column: int) -> int:
+    return row * width + column
+
+
+def get_all_neighbors(width: int, length: int, row: int, column: int) -> dict[int, [list[int]]]:
     output = defaultdict(list)
-    for i in range(width):
-        if i < column:
-            output[0].append((i, row))
+    for c in range(width):
+        index = get_index(width, row, c)
+        if c < column:
+            output[0].insert(0, index)
+        if c > column:
+            output[1].append(index)
 
-    for i in range(width):
-        if i > column:
-            output[1].append((i, row))
-
-    for j in range(length):
-        if j < row:
-            output[2].append((column, j))
-    for j in range(length):
-        if j > row:
-            output[3].append((column, j))
-
-    output[0] = list(reversed(output[0]))
-    output[2] = list(reversed(output[2]))
+    for r in range(length):
+        index = get_index(width, r, column)
+        if r < row:
+            output[2].insert(0, index)
+        if r > row:
+            output[3].append(index)
     return output
 
 
-def treehouse_2(board, width, length):
+def treehouse_2(board: list[int], width: int, length: int) -> int:
     max_score = 0
     for column in range(width):
-        for row in range(length - 1):
-            index = row * width + column
-            value = board[index]
-            neighbors = get_all_neighbors(width, length - 1, row, column)
+        for row in range(length):
+            value = board[get_index(width, row, column)]
+            neighbors = get_all_neighbors(width, length, row, column)
 
             if len(neighbors) < 4:
                 continue
 
             product = 1
             for side in neighbors.values():
-                for i, (c, r) in enumerate(side):
-                    neighbor_value = board[r * width + c]
+                for count, index in enumerate(side):
+                    neighbor_value = board[index]
                     if neighbor_value >= value:
                         break
-                product *= i + 1
+                product *= count + 1
             max_score = max(max_score, product)
     return max_score
 
 
-def treehouse_1(board, width, length):
+def treehouse_1(board: list[int], width: int, length: int) -> int:
     total = 0
     for column in range(width):
-        for row in range(length - 1):
-            index = row * width + column
-            value = board[index]
-            neighbors = get_all_neighbors(width, length - 1, row, column)
+        for row in range(length):
+            value = board[get_index(width, row, column)]
+            neighbors = get_all_neighbors(width, length, row, column)
 
             visible = False
             if len(neighbors) < 4:
@@ -87,8 +87,8 @@ def treehouse_1(board, width, length):
 
             for side in neighbors.values():
                 taller_exists = False
-                for c, r in side:
-                    neighbor_value = board[r * width + c]
+                for index in side:
+                    neighbor_value = board[index]
                     if neighbor_value >= value:
                         taller_exists = True
                 if not taller_exists:
