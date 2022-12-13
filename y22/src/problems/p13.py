2f6fa210b9aa8e47907f823058112b692310102c
Marcos Gaeta <mgaeta89@gmail.com>
2022-12-12 23:06:21 -0800

    some clarity

--- y22/src/problems/p13.py
+++ y22/src/problems/p13.py
@@ -40,6 +40,7 @@ def get_decoder_key(sorted_packets: list[any]) -> int:
 def compare(left: list[any] | int, right: list[any] | int, verbose=False) -> int:
     if verbose:
         print(left, right)
+
     if type(left) == int and type(right) == int:
         if verbose:
             print("int,int:", left, right, right - left)
@@ -49,53 +50,43 @@ def compare(left: list[any] | int, right: list[any] | int, verbose=False) -> int
     elif type(left) == int and type(right) == list:
         return compare([left], right, verbose=verbose)
     elif type(left) == list and type(right) == list:
-        # march
         if verbose:
             print("march", left, right)
-        min_length = min(len(left), len(right))
-        for i in range(min_length):
+
+        for i in range(min(len(left), len(right))):
             cmp = compare(left[i], right[i], verbose=verbose)
             if verbose:
                 print("cmp:", cmp)
-            if cmp >= 1:
-                return 1
-            elif cmp <= -1:
-                return -1
-            else:
-                # same
-                if verbose:
-                    print("same", left[i], right[i])
-                pass
-        if len(left) < len(right):
-            return 1
-        elif len(left) > len(right):
-            return -1
-        else:
-            return 0
+            if cmp:
+                return cmp
+
+            # cmp == 0 when they're the same
+            if verbose:
+                print("same", left[i], right[i])
+        return len(right) - len(left)
     else:
         raise Exception("bad", type(left), type(right))
 
 
-def get_valid_pairs(raw_input) -> int:
+def get_valid_pairs(raw_input, verbose=False) -> int:
     output = []
     pairs = parse_inputs(raw_input)
     for i, (left, right) in enumerate(pairs):
-        if compare(left, right) > 0:
+        if compare(left, right, verbose=verbose) > 0:
             output.append(i + 1)
+    if verbose:
+        print(output)
     return sum(output)
 
 
 def parse_inputs(raw_input) -> list[tuple[int, int]]:
     output = []
     left = None
-    right = None
     for i, row in enumerate(raw_input):
         if i % 3 == 0:
             left = row
         if i % 3 == 1:
-            right = row
-        if i % 3 == 1:
-            output.append((json.loads(left), json.loads(right)))
+            output.append((json.loads(left), json.loads(row)))
     return output
 
 
