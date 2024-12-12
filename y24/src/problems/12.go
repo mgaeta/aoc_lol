3f9e6b38ca0762ca79c38e9da37cacac918ad6bb
Marcos Gaeta <mgaeta89@gmail.com>
2024-12-12 13:05:05 -0800

    2024.12.2

--- y24/src/problems/12.go
+++ y24/src/problems/12.go
@@ -47,7 +47,7 @@ func Solve12a(data string, verbose bool) string {
 	// collect all possible letters?
 	// create n*n regions and merge them?
 
-	regionsById, pointsToRegions, err := generateRegions(board, width, height)
+	regionsById, pointsToRegions, err := generateRegions(board, width, height, verbose)
 	if err != nil {
 		panic(err)
 	}
@@ -102,46 +102,11 @@ func calculateResult(
 	return area * perimeter
 }
 
-func calculateResult2(
-	pointsToRegions map[string]int,
-	region Region,
-	width int,
-	height int,
-	verbose bool,
-) int {
-	area := 0
-	perimeter := 0
-	for _, point := range region.Points.ToSlice() {
-		p := 4
-		x, y, err := utils.DecodeTuple(point)
-		if err != nil {
-			panic(err)
-		}
-		neighbors := ListNeighbors12(width, height, x, y)
-		for _, neighborPoint := range neighbors {
-			neighborRegionId, ok := pointsToRegions[neighborPoint]
-			if !ok {
-				panic("couldn't find region for point")
-			}
-
-			if neighborRegionId == region.Id {
-				p--
-			}
-		}
-		perimeter += p
-		area++
-	}
-
-	if verbose {
-		fmt.Println("area", area, "perimeter", perimeter)
-	}
-	return area * perimeter
-}
-
 func generateRegions(
 	board [][]string,
 	width int,
 	height int,
+	verbose bool,
 ) (
 	map[int]Region,
 	map[string]int,
@@ -177,15 +142,19 @@ func generateRegions(
 			)
 			for _, neighborPoint := range neighbors {
 				region := regionsById[regionId]
-				fmt.Println(
-					region.Symbol,
-					"region.Points",
-					region.Points,
-					"neighborPoint",
-					neighborPoint,
-				)
+				if verbose {
+					fmt.Println(
+						region.Symbol,
+						"region.Points",
+						region.Points,
+						"neighborPoint",
+						neighborPoint,
+					)
+				}
 				if region.Points.Contains(neighborPoint) {
-					fmt.Println("region.Points.Contains(neighborPoint)")
+					if verbose {
+						fmt.Println("region.Points.Contains(neighborPoint)")
+					}
 					continue
 				}
 				otherRegionId, ok := pointsToRegions[neighborPoint]
@@ -198,29 +167,35 @@ func generateRegions(
 					return nil, nil, fmt.Errorf("invalid region ID")
 				}
 
-				fmt.Println(
-					"region.Symbol",
-					region.Symbol,
-					region.Id,
-					"otherRegion.Symbol",
-					otherRegion.Symbol,
-					otherRegion.Id,
-				)
+				if verbose {
+					fmt.Println(
+						"region.Symbol",
+						region.Symbol,
+						region.Id,
+						"otherRegion.Symbol",
+						otherRegion.Symbol,
+						otherRegion.Id,
+					)
+				}
 				if otherRegion.Symbol == region.Symbol {
 					newPoints := region.Points.Union(otherRegion.Points)
-					fmt.Println("newPoints", newPoints)
+					if verbose {
+						fmt.Println("newPoints", newPoints)
+					}
 					regionsById[regionId] = Region{
 						Id:     regionId,
 						Points: newPoints,
 						Symbol: region.Symbol,
 					}
-					fmt.Println(
-						"deleting from regions by id",
-						otherRegion.Id,
-						"in favor of",
-						region.Symbol,
-						region.Id,
-					)
+					if verbose {
+						fmt.Println(
+							"deleting from regions by id",
+							otherRegion.Id,
+							"in favor of",
+							region.Symbol,
+							region.Id,
+						)
+					}
 					delete(regionsById, otherRegion.Id)
 					// I have to do this for every point in the region
 					for _, otherPoint := range otherRegion.Points.ToSlice() {
@@ -233,6 +208,99 @@ func generateRegions(
 	return regionsById, pointsToRegions, nil
 }
 
+func countCorners(
+	region Region,
+	pointsToRegions map[string]int,
+	verbose bool,
+) int {
+	output := 0
+	for _, point := range region.Points.ToSlice() {
+		x, y, err := utils.DecodeTuple(point)
+		if err != nil {
+			panic(err)
+		}
+		neighbors := ListNeighbors12Part2(
+			pointsToRegions,
+			x,
+			y,
+			region.Id,
+		)
+		fmt.Println(
+			region.Symbol,
+			neighbors,
+		)
+		for i := 0; i < len(neighbors); i += 2 {
+			edge0 := neighbors[(i+7)%8]
+			edge1 := neighbors[(i+1)%8]
+			corner := neighbors[i]
+			if verbose {
+				fmt.Println(
+					i,
+					(i+7)%8,
+					(i+1)%8,
+					"=",
+					edge0,
+					edge1,
+					corner,
+				)
+			}
+
+			// If all false-??-false, we have an outer corner
+			if !edge0 && !edge1 {
+				output += 1
+				continue
+			}
+			// If it's true-false-true, we have an inner corner
+			if edge0 && edge1 && !corner {
+				output += 1
+			}
+		}
+	}
+	return output
+}
+
+func calculateResult2(
+	pointsToRegions map[string]int,
+	region Region,
+	verbose bool,
+) int {
+	area := region.Points.Cardinality()
+	perimeter := countCorners(region, pointsToRegions, verbose)
+	if verbose {
+		fmt.Println(region.Symbol, "area", area, "perimeter", perimeter)
+	}
+	return area * perimeter
+}
+
+func ListNeighbors12Part2(
+	pointsToRegions map[string]int,
+	x int,
+	y int,
+	id int,
+) []bool {
+	output := make([]bool, 0)
+	for deltaX := -1; deltaX <= 1; deltaX++ {
+		for deltaY := -1; deltaY <= 1; deltaY++ {
+			token := utils.EncodeTuple(x+deltaX, y+deltaY)
+			found, ok := pointsToRegions[token]
+			output = append(output, ok && found == id)
+		}
+	}
+	// 036
+	// 147
+	// 258
+	return []bool{
+		output[0],
+		output[1],
+		output[2],
+		output[5],
+		output[8],
+		output[7],
+		output[6],
+		output[3],
+	}
+}
+
 func Solve12b(data string, verbose bool) string {
 	output := 0
 	lines := utils.GetStrings(data)
@@ -243,7 +311,7 @@ func Solve12b(data string, verbose bool) string {
 	}
 	// collect all possible letters?
 	// create n*n regions and merge them?
-	regionsById, pointsToRegions, err := generateRegions(board, width, height)
+	regionsById, pointsToRegions, err := generateRegions(board, width, height, false)
 	if err != nil {
 		panic(err)
 	}
@@ -252,8 +320,6 @@ func Solve12b(data string, verbose bool) string {
 		output += calculateResult2(
 			pointsToRegions,
 			region,
-			width,
-			height,
 			verbose,
 		)
 	}
